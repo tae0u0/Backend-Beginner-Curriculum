29f992356ba89b3586221e93e478dd1831685199
taeyeongkims <xodud8530@gmail.com>
2025-08-04 22:45:33 +0900

    mvc version2 commit

---
+++ week2/mvc-version2/webServlet/src/main/java/com/gdgoc/webServlet/servlet/response/ResponseHtmlServlet.java
@@ -0,0 +1,28 @@
+package com.gdgoc.webServlet.servlet.response;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "requestParamServlet", urlPatterns = "/request-param")
+public class ResponseTextServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("단일 파라미터 조회");
+        String singleName = request.getParameter("name");
+        System.out.println(singleName);
+
+        System.out.println("복수 파라미터 조회");
+        String[] names = request.getParameterValues("name");
+        for(String name : names){
+            System.out.println(name);
+        }
+
+        response.getWriter().write("ok");
+    }
+}
---
+++ week2/mvc-version2/webServlet/src/main/java/com/gdgoc/webServlet/servlet/testServlet.java
@@ -0,0 +1,7 @@
+package com.gdgoc.webServlet.servlet;
+
+import jakarta.servlet.annotation.WebServlet;
+
+@WebServlet(name = "userServlet", urlPatterns = "/user")
+public class UserServlet {
+}
